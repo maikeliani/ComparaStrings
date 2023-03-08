bb70f84b399d153a173ec88f8cff3c65a5c1264a
maikeliani <maikel.iani31@gmail.com>
2023-03-07 22:14:31 -0300

    Add project files.

---
+++ ComparaStrings/Program.cs
@@ -0,0 +1,29 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {   string textoA, textoB;
+
+        Console.WriteLine(" Informe o primeiro texto: ");
+        textoA = Console.ReadLine();
+
+        Console.WriteLine(" \nInforme o segundo texto para comparação: ");
+        textoB = Console.ReadLine();
+
+        Console.WriteLine(ComparaStrings(textoA, textoB));
+
+
+        string ComparaStrings(string a, string b)
+        {
+            
+            if (a.Equals(b))
+            {
+                return " As strings são iguais";
+            }
+            return " São diferentes";
+        }
+
+
+    }
+
+
+}
